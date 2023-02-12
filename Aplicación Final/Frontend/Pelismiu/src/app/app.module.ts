bfa0badfa473f5a900adf6aa7414367e68fd838a
Juan Antonio Tora Canovas <juanantonio.tora@gmail.com>
2023-02-12 18:11:43 +0100

    Instalación de componentes y librería bootstrap
    Se carga la nueva dependencia Bootstrap en el Angular.js dentro de la etiqueta Styles (linea 27).

--- Aplicación Final/Frontend/Pelismiu/src/app/app.module.ts
+++ Aplicación Final/Frontend/Pelismiu/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentesIndependientes/login/login.component';
+import { RegistrarUsuarioComponent } from './componentesIndependientes/registrar-usuario/registrar-usuario.component';
+import { RecuperarUsuarioComponent } from './componentesIndependientes/recuperar-usuario/recuperar-usuario.component';
+import { DashboardComponent } from './componentesIndependientes/dashboard/dashboard.component';
+import { VerificarCorreoComponent } from './componentesIndependientes/verificar-correo/verificar-correo.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarUsuarioComponent,
+    RecuperarUsuarioComponent,
+    DashboardComponent,
+    VerificarCorreoComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
---
+++ Aplicación Final/Frontend/Pelismiu/src/app/componentesIndependientes/recuperar-usuario/recuperar-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperar-usuario',
+  templateUrl: './recuperar-usuario.component.html',
+  styleUrls: ['./recuperar-usuario.component.css']
+})
+export class RecuperarUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
