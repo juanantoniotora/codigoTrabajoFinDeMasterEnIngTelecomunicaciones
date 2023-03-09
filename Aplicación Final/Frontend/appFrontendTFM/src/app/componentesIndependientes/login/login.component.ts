c92fe659c51ffd0c04ba5e14b834f8b3efbe22e7
Juan Antonio Tora Canovas <juanantonio.tora@gmail.com>
2023-03-09 13:40:56 +0100

    Revert "Ejecución NG SERVE vuelve a funcionar sobre proyecto inicial"
    This reverts commit 9ed14f7f179078edee01e180e2f5a08104eeeed3.

---
+++ Aplicación Final/Frontend/appFrontendTFM/src/app/componentesIndependientes/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loading : boolean = false;
+  mostrarMensajeErrorRegistro : boolean = false;
+  mostrarMensajeExitoRegistro : boolean = false;
+  mensajeInfo : String = "";
+  loginUsuario : FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private afAuth : AngularFireAuth,
+    private router : Router
+  ) {
+    this.loginUsuario = this.fb.group({
+      email : ['', [Validators.required, Validators.email]],
+      password : ['', Validators.required],
+    })
+    this.loading = false;
+    this.mostrarMensajeErrorRegistro = false;
+    this.mensajeInfo = "";
+   }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    const email = this.loginUsuario.value.email;
+    const password = this.loginUsuario.value.password;
+
+    this.loading = true;
+
+    this.afAuth.signInWithEmailAndPassword(email, password).then((user)=>{
+      console.log(user)
+      if(user.user?.emailVerified){
+        this.router.navigate(['/dashboard'])
+      }
+      else{
+        this.router.navigate(['/verificar-correo'])
+      }
+    }).catch((error)=>{
+      this.loading = false;
+      console.log('error');
+      this.loading=false;
+      this.mostrarMensajeErrorRegistro=true;
+      this.mostrarMensajeExitoRegistro=false;
+      this.mensajeInfo=this.firebaseError(error.code)
+    })
+  }
+
+  firebaseError(code:String){
+    switch(code){
+      // password invalida
+      case 'auth/invalid-password':
+        return 'Contraseña inválida'
+      // password incorrecta
+      case 'auth/wrong-password':
+        return 'Contraseña incorrecta'
+      // correo invalido
+      case 'auth/invalid-email':
+        return 'Correo inválido'
+      case 'auth/user-not-found':
+        return 'El usuario no existe'
+      // cualquier otro error no contemplado
+      default: 
+        return 'Email/Password incorrecto'
+    }
+  }
+
+}
---
+++ Aplicación Final/Frontend/appFrontendTFM/src/app/componentesIndependientes/recuperar-password/recuperar-password.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recuperar-password',
+  templateUrl: './recuperar-password.component.html',
+  styleUrls: ['./recuperar-password.component.css']
+})
+export class RecuperarPasswordComponent implements OnInit {
+
+  loading : boolean = false;
+  mostrarMensajeErrorRegistro : boolean = false;
+  mostrarMensajeExitoRegistro : boolean = false;
+  mensajeInfo : String = "";
+  recuperarUsuario : FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private afAuth : AngularFireAuth,
+    private router : Router
+  ) {
+    this.recuperarUsuario = this.fb.group({
+      correo : ['', [Validators.required, Validators.email]],
+    })
+    this.loading = false;
+    this.mostrarMensajeErrorRegistro = false;
+    this.mensajeInfo = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+  recuperar(){
+    console.log(this.recuperarUsuario)
+    const email = this.recuperarUsuario.value.correo;
+    console.log(email);
+    this.loading = true;
+    this.afAuth.sendPasswordResetEmail(email).then(async ()=>{
+      console.log("exito recuperarPassword()");
+      this.mostrarMensajeErrorRegistro=false;
+      this.mostrarMensajeExitoRegistro=true;
+      this.mensajeInfo="Correo enviado"
+      await this.delay(1000);
+      this.loading=false; 
+      await this.delay(2000);
+      this.router.navigate(['/login'])
+      
+    }).catch((error)=>{
+      console.log('error recuperarPassword()');
+      this.loading=false;  
+      this.mostrarMensajeErrorRegistro=true;
+      this.mostrarMensajeExitoRegistro=false;
+      this.mensajeInfo=this.firebaseError(error.code)
+    })
+  }
+
+  firebaseError(code:String){
+    switch(code){
+      // password invalida
+      case 'auth/invalid-password':
+        return 'Contraseña inválida'
+      // password incorrecta
+      case 'auth/wrong-password':
+        return 'Contraseña incorrecta'
+      // correo invalido
+      case 'auth/invalid-email':
+        return 'Correo inválido'
+      case 'auth/user-not-found':
+        return 'El usuario no existe'
+      // cualquier otro error no contemplado
+      default: 
+        return 'Email/Password incorrecto'
+    }
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+}
