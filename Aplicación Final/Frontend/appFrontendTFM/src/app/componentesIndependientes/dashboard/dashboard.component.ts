21b6d7fd246c5f6dbacff13eac63908bd29b862e
Juan Antonio Tora Canovas <juanantonio.tora@gmail.com>
2023-03-09 11:40:50 +0100

    WIP-Nuevo Dashboard con botón LogOut funcionando

--- Aplicación Final/Frontend/appFrontendTFM/src/app/componentesIndependientes/dashboard/dashboard.component.ts
+++ Aplicación Final/Frontend/appFrontendTFM/src/app/componentesIndependientes/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(   
+     private afAuth : AngularFireAuth,
+     private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  logout(){
+    this.afAuth.signOut().then(()=>{
+      console.log("LogOut realizado.")
+      this.router.navigate(["/login"])
+    })
+    .catch((error)=>{console.log("Error en LogOut")})
   }
 
 }
